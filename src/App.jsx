ed22728c3fe4d07454b277d23ee1dd011d644bf3
abudi-09 <abdsalih229@gmail.com>
2025-04-23 21:17:11 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./component/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./page/Home/Home";
+import Cart from "./page/Cart/Cart";
+import PlaceOrder from "./page/PlaceOrder/PlaceOrder";
+import "./App.css";
+import Header from "./component/Header/Header";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+import { assets } from "../../assets/assets";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2> order your favourite food here </h2>
+        <p>
+          choose A different type of header HTTP headers, a table header, or
+          another UI component please specify.
+        </p>
+        <button> Veiw Menu </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
